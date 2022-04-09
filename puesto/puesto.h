5f7690baafe79925f7e5435a0be9c84b77c848de
iness <iness@DESKTOP-T3BES9L>
2022-04-09 16:41:06 +0200

    p

--- puesto/puesto.h
+++ puesto/puesto.h
@@ -1,11 +1,13 @@
 #ifndef PUESTO_PUESTO_H_
 #define PUESTO_PUESTO_H_
 
+#include "../sqlite3/sqlite3.h"
+
 #define MAX_PUESTOS 20
 
 typedef struct
 {
-	int dod;
+	int cod;
 	char *marca;
 	int coste;
 
@@ -13,10 +15,14 @@ typedef struct
 
 typedef struct
 {
-	int numnumPuestos;
+	int numPuestos;
 	Puesto *puestos;
 
 } ListaPuestos;
 
+int pedirCodigoPuesto();
+void pedirDatosPuesto(sqlite3 *db, Puesto *p);
+void insertarPuesto(sqlite3 *db, Puesto *p);
+void eliminarPuesto(sqlite3 *db, int cod);
 
 #endif /* PUESTO_PUESTO_H_ */
